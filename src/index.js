2c487bbf56e462a5fb20740996306e003cda4753
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-13 08:16:57 -0700

    Installed bootstrap/imported. Installed axios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css'
 
-reactDom.render(document.getElementById('root'));
+reactDom.render(<App />, document.getElementById('root'));
